de3a2177314e075bf6901cc9a6ae6ba2c508cc4b
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-12-22 13:50:09 +0300

    init

---
+++ MainWindow.py
@@ -0,0 +1,53 @@
+import gi
+gi.require_version("Gtk", "3.0")
+
+from gi.repository import Gtk, GLib
+
+class MainWindow(object):
+    def __init__(self, application=None):
+        self.Application = application
+        builder = Gtk.Builder.new_from_file("main.ui")
+        win = builder.get_object("ui_main_window")
+        self.pin = builder.get_object("ui_main_pin")
+        self.numpad = builder.get_object("ui_main_numpad")
+
+        self.cur = ""
+
+        if application:
+            win.set_application(application)
+
+        for i in range(0,10):
+            but = builder.get_object(f"ui_main_n{i}")
+            but.connect("clicked", self.num_event, str(i))
+
+        delete = builder.get_object("ui_main_del")
+        delete.connect("clicked", self.del_event)
+        self.pin.set_text(self.format_cur())
+        win.fullscreen()
+        win.show_all()
+
+    def check(self):
+        print(self.cur)
+        self.cur = ""
+        self.pin.set_text(self.format_cur())
+        self.numpad.set_sensitive(True)
+
+    def format_cur(self):
+        cur = (6-len(self.cur))*"-"+self.cur
+        return cur[:3]+" "+ cur[3:]
+
+    def num_event(self, widget, i):
+        self.cur += i
+        self.pin.set_text(self.format_cur())
+        if len(self.cur) == 6:
+            self.numpad.set_sensitive(False)
+            GLib.timeout_add(2000, self.check)
+
+
+    def del_event(self,widget):
+        self.cur = self.cur[:-1]
+        self.pin.set_text(self.format_cur())
+
+if __name__ == "__main__":
+    main = MainWindow()
+    Gtk.main()
