ddf27fe65b7baac48bd354ca5c97da1a5743b928
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-12-25 17:40:04 +0300

    otp pam module test

---
+++ pam/pam_otp.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import os
+import pwd
+import grp
+import traceback
+import pyotp
+import json
+
+"""
+{
+    "etapadmin" : "JBSWY3DPEHPK3PXP"
+}
+"""
+
+## Pam config (common-auth)
+## after pam_unix:
+# auth sufficient pam_python.so /usr/libexec/pam_otp.py
+
+
+def pam_sm_authenticate(pamh, flags, argv):
+    # fetch username
+    try:
+        user = pamh.get_user(None)
+        passwd = pamh.authtok
+    except Exception as e:
+        return pamh.PAM_AUTH_ERR
+
+    # read config
+    config = {}
+    if not os.path.isfile("/etc/etap-otp"):
+        return pamh.PAM_AUTH_ERR
+    with open("/etc/etap-otp", "r") as f:
+        config = json.load(f)
+
+    # check otp
+    if user in config:
+        otp = pyotp.TOTP(config[user])
+        if otp.now() == passwd:
+            return pamh.PAM_SUCCESS
+    return pamh.PAM_AUTH_ERR
+
+def pam_sm_setcred(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
